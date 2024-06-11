7941c9067c7fac1075cb0df9c77ffe155fd35e72
saddam-satria <ardhisaddam@gmail.com>
2024-06-11 18:46:57 +0700

    change: auth header key

--- middlewares/authentication.go
+++ middlewares/authentication.go
@@ -1,27 +1,44 @@
 package middlewares
 
 import (
+	"strings"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/saddam-satria/posq-be/commons"
 	"github.com/saddam-satria/posq-be/domains/apis"
 )
 
 func Authentication(ctx *fiber.Ctx) error {
-	header:= new(apis.AuthHeader)
+	header := new(apis.AuthHeader)
 
-	
-	if err:= ctx.ReqHeaderParser(header); err!=nil{
+	if err := ctx.ReqHeaderParser(header); err != nil {
 		ctx.SendStatus(fiber.StatusUnauthorized)
-		ctx.JSON(commons.GetResponse[any]("header error", fiber.StatusUnauthorized,nil))
+		ctx.JSON(commons.GetResponse[any]("header error", fiber.StatusUnauthorized, nil))
 	}
 
-	token:= header.Authorization
-	
-	_, err:= commons.VerifyToken(token) 
+	token := header.Authorization
+
+	tokenParsed := strings.Split(token, " ")
+
+	if len(tokenParsed) != 2 {
+		ctx.SendStatus(fiber.StatusUnauthorized)
+		ctx.JSON(commons.GetResponse[any]("access denied", fiber.StatusUnauthorized, nil))
+		return nil
+	}
+
+	if tokenParsed[0] != commons.GoDotEnv("AUTH_HEADER_KEY") {
+		ctx.SendStatus(fiber.StatusUnauthorized)
+		ctx.JSON(commons.GetResponse[any]("access denied", fiber.StatusUnauthorized, nil))
+		return nil
+	}
+
+	jwtToken := tokenParsed[1]
+
+	_, err := commons.VerifyToken(jwtToken)
 
 	if err != nil {
 		ctx.SendStatus(fiber.StatusUnauthorized)
-		ctx.JSON(commons.GetResponse(err.Error(), fiber.StatusUnauthorized,token))
+		ctx.JSON(commons.GetResponse[any](err.Error(), fiber.StatusUnauthorized, nil))
 		return nil
 	}
 
--- utils/routes.go
+++ utils/routes.go
@@ -3,10 +3,12 @@ package utils
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/saddam-satria/posq-be/services/auth"
+	"github.com/saddam-satria/posq-be/services/product"
 	"github.com/saddam-satria/posq-be/services/user"
 )
 
 func GetRoute(route *fiber.App) {
 	auth.AuthRoute(route)
 	user.UserRoute(route)
+	product.ProductRoute(route)
 }
